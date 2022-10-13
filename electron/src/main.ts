d531694dc9fd4c2a6a0474e47a115b02d945aa2f
livemehere <rhdxoals@gmail.com>
2022-10-13 17:49:08 +0900

    devtool setting

--- electron/src/main.ts
+++ electron/src/main.ts
@@ -1,4 +1,5 @@
 import {app, BrowserWindow} from 'electron';
+import * as path from "path";
 
 const WIDTH = 1920;
 const HEIGHT = 1080;
@@ -11,7 +12,6 @@ class Main {
 
     private constructor() {
         this.ready();
-        console.log('node env',process.env.NODE_ENV)
     }
 
     static init(){
@@ -21,12 +21,25 @@ class Main {
     private createWindow(){
         this.win = new BrowserWindow({
             width:WIDTH,
-            height:HEIGHT
+            height:HEIGHT,
+            webPreferences:{
+                preload:path.join(__dirname, '../dist/preload.js')
+            }
         })
         this.win.loadURL('http://127.0.0.1:5173');
-        if(isDev){
-            this.win.webContents.openDevTools();
-        }
+
+        isDev && this.win.webContents.openDevTools();
+
+        // for windows
+        this.app.on('window-all-closed',()=>{
+            if(process.platform !== 'darwin') this.app.quit();
+        })
+
+        // for macOS
+        this.app.on('activate',()=>{
+            if(BrowserWindow.getAllWindows().length === 0) this.createWindow();
+        })
+
     }
 
     private ready(){
